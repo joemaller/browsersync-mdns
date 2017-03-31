f30237489e8e673be2bee4a14b9286bea13b50d5
Joe Maller <joe@ideasonpurpose.com>
2017-03-31 12:08:03 -0400

    add hostname fallback

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const chalk = require('chalk');
 const mdns = require('mdns');
 const readPkgUp = require('read-pkg-up').sync;
@@ -8,7 +9,7 @@ const pluginName = 'Browsersync mDNS';
 module.exports = {
   'plugin:name': pluginName,
   plugin: function(options, browserSync) {
-    const displayName = options.name || pkg.name;
+    const displayName = options.name || pkg.name || os.hostname() ;
 
     mdns.createAdvertisement(mdns.tcp('http'), browserSync.options.get('port'), {
       name: displayName
