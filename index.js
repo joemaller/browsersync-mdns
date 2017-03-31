988fa2d6238da7044413525b202eb60c796433fa
Joe Maller <joe@ideasonpurpose.com>
2017-03-31 11:11:08 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
+const chalk = require('chalk');
+const mdns = require('mdns');
+const pkg = readPkgUp({ normalize: false }).pkg;
 const readPkgUp = require('read-pkg-up').sync;
 
-const pkg = readPkgUp({ normalize: false }).pkg;
-const mdns = require('mdns');
-const chalk = require('chalk');
 const pluginName = 'Browsersync mDNS';
 
 module.exports = {
   'plugin:name': pluginName,
   plugin: function(options, browserSync) {
-
-        // console.log('************');
-        // console.log(process.cwd());
-        // console.log(pkg);
-        // console.log(boo);
-        // console.log('options', options);
-        // console.log('bs', bs);
-
-        // console.log(require('./package.json'));
-        // console.log('************');
-
-        /// TODO: Merge options against pkg.json defaults
-
-        // console.log('displayName', displayName);
-
     const displayName = options.name || pkg.name;
+
+    mdns.createAdvertisement(mdns.tcp('http'), browserSync.options.get('port'), {
+      name: displayName
+    });
+
     browserSync.logger.info(
       '[' + chalk.blue('mDNS') + ']',
       'Advertising',
       chalk.cyan(displayName)
     );
-    mdns.createAdvertisement(mdns.tcp('http'), browserSync.options.get('port'), {
-      name: displayName
-    });
   }
 };
