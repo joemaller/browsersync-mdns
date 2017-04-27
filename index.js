ccafad503e7440143a6bfdfa0478513d4d54ca2a
Joe Maller <joe@ideasonpurpose.com>
2017-04-26 23:18:14 -0400

    update announcement. tests. Closes #1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const chalk = require('chalk');
+const leftPad = require('left-pad');
 const mdns = require('mdns');
 const readPkgUp = require('read-pkg-up').sync;
 
@@ -8,17 +8,44 @@ const pluginName = 'Browsersync mDNS';
 
 module.exports = {
   'plugin:name': pluginName,
-  plugin: function(options, browserSync) {
-    const displayName = options.name || pkg.name || os.hostname() ;
+  plugin: function(options, browsersync) {
+    const displayName = options.name || pkg.name || os.hostname();
 
-    mdns.createAdvertisement(mdns.tcp('http'), browserSync.options.get('port'), {
+    mdns.createAdvertisement(mdns.tcp('http'), browsersync.options.get('port'), {
       name: displayName
     });
 
-    browserSync.logger.info(
-      '[' + chalk.blue('mDNS') + ']',
-      'Advertising',
-      chalk.cyan(displayName)
-    );
+    browsersync.emitter.on('service:running', function() {
+      logAdvertisement(displayName, browsersync);
+    });
   }
 };
+
+function logAdvertisement(name, bs) {
+  const urls = bs.options.get('urls').toJS();
+  const keys = Object.keys(urls);
+  let longestName = 0;
+  let longesturl = 0;
+  const offset = 2;
+  let label = 'mDNS Name';
+
+
+  keys.map(function (key) {
+    if (key.length > longestName) {
+      longestName = key.length;
+    }
+    if (urls[key].length > longesturl) {
+      longesturl = urls[key].length;
+    }
+    return key;
+  });
+
+  label = (label.length < longestName) ? label : label.split()[0];
+
+  const line = new Array(longestName + offset + longesturl + 1).join('-');
+
+  bs.logger.info('{bold:Advertising:}');
+  bs.logger.unprefixed('info', '{grey: %s}', line);
+  bs.logger.unprefixed('info', ' %s: {magenta:%s}', leftPad(label, longestName), name);
+  bs.logger.unprefixed('info', '{grey: %s}', line);
+}
--- test/index.js
+++ test/index.js
@@ -7,6 +7,9 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 
+const { Map } = require('immutable');
+const EventEmitter = require('events');
+
 chai.should();
 chai.use(sinonChai);
 
@@ -15,7 +18,10 @@ const os = require('os');
 const mdns = require('mdns');
 const readPkgUp = require('read-pkg-up');
 
-const pkg = {pkg: {name: 'package.json'}};
+const pkg = { pkg: { name: 'package.json' }};
+const bsUrlShort = { local: 'http://localhost:3000' }; // key: 5 chars, value: 21 chars
+const bsUrlLong = { a_much_longer_key: 'https://a-very-long-example-url:3001'};
+const bsUrls = Object.assign({}, bsUrlShort, bsUrlLong);
 
 describe('Browsersync mDNS plugin', function() {
 
@@ -23,8 +29,12 @@ describe('Browsersync mDNS plugin', function() {
     this.mdns = sinon.stub(mdns, 'createAdvertisement');
     this.readPkgUp = sinon.stub(readPkgUp, 'sync').returns(pkg);
     this.os = sinon.stub(os, 'hostname');
-    this.bs = {logger: {info: sinon.spy()}, options: new Map()};
-    this.bs.options.set('port', 3000);
+    this.bs = {
+      logger: {info: sinon.spy(), unprefixed: sinon.spy()},
+      options: Map({port: 3000, urls: Map(bsUrls)}),
+      emitter: new EventEmitter
+    };
+    // this.bs.options.set('urls', bsUrls);
     this.bsmdns = proxyquire('../', {
       'mdns': this.mdns,
       'read-pkg-up': this.readPkgUp
@@ -49,13 +59,14 @@ describe('Browsersync mDNS plugin', function() {
 
   it('should advertise a service', function() {
     this.bsmdns.plugin({}, this.bs);
+    this.bs.emitter.emit('service:running');
     this.bs.logger.info.should.have.been.called;
     this.mdns.args.should.have.deep.property('[0][1]', 3000);
   });
 
   it('should use the provided port', function() {
-    const port = 5000
-    this.bs.options.set('port', port);
+    const port = 5000;
+    this.bs.options = Map({port: port, urls: Map(bsUrls)});
     this.bsmdns.plugin({}, this.bs);
     this.mdns.args.should.have.deep.property('[0][1]', port);
   });
@@ -85,5 +96,22 @@ describe('Browsersync mDNS plugin', function() {
     bsmdns.plugin({}, this.bs);
 
     this.mdns.args.should.have.deep.property('[0][2].name', hostname);
+  });
+
+
+  it('should log underlines to match length of Browsersync Access Urls', function() {
+    this.bs.options = Map({port: 3000, urls: Map(bsUrlShort)});
+    this.bsmdns.plugin({}, this.bs);
+    this.bs.emitter.emit('service:running');
+
+    this.bs.options = Map({port: 3000, urls: Map(bsUrlLong)});
+    this.bsmdns.plugin({}, this.bs);
+    this.bs.emitter.emit('service:running');
+
+    this.bs.logger.info.should.have.been.called;
+    this.bs.logger.unprefixed.getCall(0).args[2].should.have.lengthOf(28);
+    this.bs.logger.unprefixed.getCall(0).args[2].should.be.string('----------------------------');
+    this.bs.logger.unprefixed.getCall(3).args[2].should.have.lengthOf(55);
+    this.bs.logger.unprefixed.getCall(3).args[2].should.be.string('-------------------------------------------------------');
   });
 });
